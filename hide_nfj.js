1149036f2c79768342764ab6fc6a87de134317b7
kjura <30084359+kjura@users.noreply.github.com>
2024-04-28 16:08:06 +0200

    refactor: put code inside a function for calls

--- hide_nfj.js
+++ hide_nfj.js
@@ -1,32 +1,36 @@
-const adjusted_salaries_class_name = 
-    ".text-truncate.badgy.salary.lg"
-    + "\\:tw-btn.tw-text-ink.lg"
-    + "\\:tw-btn-secondary-outline.tw-text-xs.lg"
-    + "\\:tw-py-0"
-    + "\\.5.lg"
-    + "\\:tw-px-2.ng-star-inserted";
+function hide_me() {
 
-const webpage_ad_class_node_list = document.querySelectorAll(".list-container.ng-star-inserted");
+    const adjusted_salaries_class_name = 
+        ".text-truncate.badgy.salary.lg"
+        + "\\:tw-btn.tw-text-ink.lg"
+        + "\\:tw-btn-secondary-outline.tw-text-xs.lg"
+        + "\\:tw-py-0"
+        + "\\.5.lg"
+        + "\\:tw-px-2.ng-star-inserted";
 
-for (const list_container of webpage_ad_class_node_list) {
+    const webpage_ad_class_node_list = document.querySelectorAll(".list-container.ng-star-inserted");
 
-    const listItems = list_container.children;
-    const listArry = Array.from(listItems);
+    for (const list_container of webpage_ad_class_node_list) {
 
-    listArry.forEach(
-        (item) => {
-             const buka = item.querySelector(adjusted_salaries_class_name);
+        const listItems = list_container.children;
+        const listArry = Array.from(listItems);
 
-             let temp = buka.textContent;
-             temp = temp.replace(/\s+/g, '');
-             let starting_range = parseInt(
-                temp.split('-')
-                );
+        listArry.forEach(
+            (item) => {
+                const buka = item.querySelector(adjusted_salaries_class_name);
 
-            if (starting_range > 20000 || starting_range === "Check Salary Match") {
-                    item.style.display = "none";
+                let temp = buka.textContent;
+                temp = temp.replace(/\s+/g, '');
+                let starting_range = parseInt(
+                    temp.split('-')
+                    );
+
+                if (starting_range > 20000 || starting_range === "Check Salary Match") {
+                        item.style.display = "none";
+                    }
                 }
-            }
-        );
+            );
+
+    }
 
 }
