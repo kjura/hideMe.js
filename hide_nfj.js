2c3b69b8b6064807cd513159df621ea7e0c10b28
kjura <30084359+kjura@users.noreply.github.com>
2024-04-28 14:44:33 +0200

    refactor: put code into void function

--- hide_nfj.js
+++ hide_nfj.js
@@ -1,30 +1,28 @@
-var adjusted_salaries_class_name = ".text-truncate.badgy.salary.lg\\:tw-btn.tw-text-ink.lg\\:tw-btn-secondary-outline.tw-text-xs.lg\\:tw-py-0\\.5.lg\\:tw-px-2.ng-star-inserted";
+void function hide_me() {
+    const adjusted_salaries_class_name = 
+    ".text-truncate.badgy.salary.lg"
+    + "\\:tw-btn.tw-text-ink.lg"
+    + "\\:tw-btn-secondary-outline.tw-text-xs.lg"
+    + "\\:tw-py-0"
+    + "\\.5.lg"
+    + "\\:tw-px-2.ng-star-inserted";
 
-var salaries_items = Array.from(
-    document.querySelectorAll(adjusted_salaries_class_name)
-);
+    const salaries_items = Array.from(
+        document.querySelectorAll(adjusted_salaries_class_name)
+    );
 
-for (const element of salaries_items) {
+    for (const element of salaries_items) {
 
-    let temp = element.textContent;
-    temp = temp.replace(/\s+/g, '');
-    let starting_range = parseInt(
-        temp.split('-')
-        );
-
-    if (starting_range > 20000) {
-        element.style.visibility = "hidden";
-    }
+        let temp = element.textContent;
+        temp = temp.replace(/\s+/g, '');
+        let starting_range = parseInt(
+            temp.split('-')
+            );
     
+        if (starting_range > 20000) {
+            element.style.visibility = "hidden";
+        }
+        
+    }
+
 }
-
-
-{/* <span _ngcontent-serverapp-c1542614857="" 
-data-cy="salary ranges on the job offer listing" nfjstopevent="" 
-commonmeasureclickevent="" nfjscrolltoponclick="" 
-class="text-truncate badgy salary lg:tw-btn tw-text-ink lg:tw-btn-secondary-outline tw-text-xs lg:tw-py-0.5 lg:tw-px-2 ng-star-inserted">
-     19&nbsp;800  – 27&nbsp;700 <!----><!----> PLN </span> */}
-
-
-// document.getElementsByClassName("text-truncate badgy salary lg:tw-btn tw-text-ink lg:tw-btn-secondary-outline tw-text-xs lg:tw-py-0.5 lg:tw-px-2 ng-star-inserted")[0]
-// text-truncate badgy salary lg\\:tw-btn tw-text-ink lg\\:tw-btn-secondary-outline tw-text-xs lg\\:tw-py-0.5 lg\\:tw-px-2 ng-star-inserted
